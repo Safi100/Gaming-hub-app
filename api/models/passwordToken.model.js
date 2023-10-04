1ca44750046f02b05134a40a657775c23f707bd9
Safi Nafi <safinafi12@gmail.com>
2023-10-04 23:31:41 +0300

    Create password token for change password

---
+++ api/models/passwordToken.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const passwordTokenSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        expires: 3600 // 1 hour
+    } 
+})
+
+module.exports = mongoose.model('PasswordToken', passwordTokenSchema)
