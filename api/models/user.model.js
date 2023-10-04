1bcf1574416ae8b83422bd2ff798d9f1e9fbb874
Safi Nafi <safinafi12@gmail.com>
2023-10-04 21:27:49 +0300

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    first_name: {
+        type: String,
+        required: [true, "Last Name is required"]
+    },
+    last_name: {
+        type: String,
+        required: [true, "First Name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    gender: {
+        type: String,
+        enum : ['Male','Female', null],
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('User', userSchema)
