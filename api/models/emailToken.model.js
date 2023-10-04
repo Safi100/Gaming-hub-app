50d8f68d4fc2989e842ce93902a2fca9e5052b1f
Safi Nafi <safinafi12@gmail.com>
2023-10-04 23:31:29 +0300

    Create email token for verification

---
+++ api/models/emailToken.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const emailTokenSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        expires: 3600 // 1 hour
+    } 
+})
+
+module.exports = mongoose.model('EmailToken', emailTokenSchema)
