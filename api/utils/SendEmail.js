57ac9c0fdc351d8f81631a8fa156ce11353a6d35
Safi Nafi <safinafi12@gmail.com>
2023-10-04 21:27:14 +0300

    Create nodemailer to send email

---
+++ api/utils/SendEmail.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+module.exports.sendEmail = async (email, subject, text) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.PASS_USER
+        }
+      });
+      var mailOptions = {
+        from: 'gamingHub.safi@gmail.com',
+        to: email,
+        subject: subject,
+        html: text
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          resolve(true); // Resolve with true when the email is sent successfully
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  });
+}
