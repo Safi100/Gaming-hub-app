26344181b853cbd64500b569b7f038829e448aad
Safi Nafi <safinafi12@gmail.com>
2023-10-05 01:01:07 +0300

    Setup some routes in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // pages
 import Register from './pages/Auth/Register';
 import Login from './pages/Auth/Login';
+import ForgotPassword from './pages/Auth/ForgotPassword';
+import ResetPassword from './pages/Auth/ResetPassword';
+import EmailVerify from './pages/Auth/EmailVerify';
 
 function App() {
   return (
       <Router>
-        <Switch>
-        <Route path="/register" exact component={<Register /> } />
-        <Route path="/login" exact component={<Login /> } />
-          {/* Add other routes here */}
-        </Switch>
+        <Routes>
+          {/* <Route path="/" exact element={} /> */} {/* Index page... later */}
+          <Route path="/register" element={<Register /> } />
+          <Route path="/login" element={<Login /> } />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:id/:token" element={<ResetPassword />} />
+          <Route path="/verify-email/:id/:token" element={<EmailVerify />} />
+          {/* <Route path='*' element={} />  */}  {/* Not found page... later */}
+           
+        </Routes>
       </Router>
   );
 }
