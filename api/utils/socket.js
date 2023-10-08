aa3c6e2ae16db1046fb8792eed3056dd875597e4
Safi Nafi <safinafi12@gmail.com>
2023-10-08 22:26:28 +0300

    setup socket.io

---
+++ api/utils/socket.js
@@ -0,0 +1,24 @@
+const socketIO = require('socket.io');
+
+// Create a function that takes an HTTP server instance as an argument
+module.exports = function (server) {
+  const io = socketIO(server, {
+    cors: {
+      origin: "http://localhost:3000", // Allow connections from this origin
+      methods: "*", // Allow all HTTP methods
+      credentials: true, // Allow passing cookies, authorization headers, etc.
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+    socket.on('disconnect', () => {
+      console.log(`Client disconnected: ${socket.id}`);
+    });
+
+    // Other socket.io event handlers...
+  });
+
+  return io;
+};
