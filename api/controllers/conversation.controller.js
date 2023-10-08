cde3c802f8ace213ff44054c472e9735d081eb1d
Safi Nafi <safinafi12@gmail.com>
2023-10-08 22:25:18 +0300

    create fetch_conversation_list

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -19,3 +19,20 @@ module.exports.Create_Group_Conversation = async (req, res, next) => {
         next(e);
     }
 }
+// get all current user's conversations that have at least one message
+module.exports.Conversation_List = async (req, res, next) => {
+    const conversations = await Conversation.find({ messages: { $exists: true, $not: { $size: 0 } }, participants: { $in: [req.user.id] }, })
+    .populate({path: 'messages', options: { sort: { timestamp: -1 }, limit: 1 }, // Sort messages by timestamp in descending order and limit to 1 message
+        populate: { // popluate sender of messages without his password
+          path: 'sender',
+          select: '-password',
+        },
+    }).exec()
+    // sort conversations by by the most recent message's timestamp
+    conversations.sort((a, b) => {
+      const timestampA = a.messages.length > 0 ? a.messages[0].timestamp : 0;
+      const timestampB = b.messages.length > 0 ? b.messages[0].timestamp : 0;
+      return timestampB - timestampA;
+    });
+    res.status(200).json(conversations);
+}
