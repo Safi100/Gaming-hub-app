5bcdad9b7d4f273cceade8dee9d91cd96476b910
Safi Nafi <safinafi12@gmail.com>
2023-10-15 16:29:53 +0300

    add avatar to user schema

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
+    avatar: {
+        url: {
+            type: String,
+        },
+        filename: {
+            type: String,
+        }
+    },
     first_name: {
         type: String,
         required: [true, "Last Name is required"]
