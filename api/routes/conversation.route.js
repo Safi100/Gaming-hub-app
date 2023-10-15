c2071c17bb7523292b952e39ab950dd5247ffa12
Safi Nafi <safinafi12@gmail.com>
2023-10-15 16:30:07 +0300

    add more routes to conversation

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { Create_Group_Conversation, create_or_fetch_private_conversation, sendMessage, Conversation_List } = require('../controllers/conversation.controller')
+const { Create_Group_Conversation, create_or_fetch_private_conversation, sendMessage, Conversation_List, fetchConversation, searchUsersForConversation } = require('../controllers/conversation.controller')
 const router = express.Router({mergeParams: true})
 
 router.post('/create-group', Create_Group_Conversation)
 router.post('/create-or-fetch-private-conversation', create_or_fetch_private_conversation)
+router.get('/conversation-list', Conversation_List)
+router.get('/:id', fetchConversation)
 router.post('/:conversationID/send-message', sendMessage)
-router.get('/conversations', Conversation_List)
 
 module.exports = router
