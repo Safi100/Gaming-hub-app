643b9bab8e907847a6342f20ab4bc61a5b522361
Safi Nafi <safinafi12@gmail.com>
2023-10-20 05:28:20 +0300

    Add more convesartion routes

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { Create_Group_Conversation, create_or_fetch_private_conversation, sendMessage, Conversation_List,
-     fetchConversation, addUserToGroup, removeUserFromGroup } = require('../controllers/conversation.controller')
+     fetchConversation, addUserToGroup, removeUserFromGroup, searchToAdd } = require('../controllers/conversation.controller')
 const { searchForUsers } = require('../controllers/user.controller');
 const router = express.Router({mergeParams: true})
 
@@ -10,8 +10,9 @@ router.get('/conversation-list', Conversation_List)
 router.post('/search-users-to-chat', searchForUsers)
 router.get('/:id', fetchConversation)
 router.post('/:conversationID/send-message', sendMessage)
+router.post('/:groupId/search-users-to-add/', searchToAdd)
 router.post('/:groupId/add-user-to-group/:userId', addUserToGroup)
-router.post('/:groupId/remove-user-from-group/:userId', removeUserFromGroup)
+router.delete('/:groupId/remove-user-from-group/:userId', removeUserFromGroup)
 // router.delete('/:groupId/delete-group/', )
 
 module.exports = router
