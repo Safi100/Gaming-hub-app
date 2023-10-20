ab7be30629b8fe4a4787ff16ec359dd7d0a03eae
Safi Nafi <safinafi12@gmail.com>
2023-10-20 05:37:18 +0300

    Create group participants list (admin can delete users)

---
+++ client/src/components/chat/GroupParticipants.jsx
@@ -0,0 +1,66 @@
+import React, {useEffect, useState, useContext} from 'react';
+import { useParams } from 'react-router-dom' 
+import CloseIcon from '@mui/icons-material/Close';
+import Avatar from '@mui/material/Avatar';
+import { stringAvatar } from '../avatar';
+import { AuthContext } from '../../context/AuthContext';
+import Axios from 'axios';
+
+const GroupParticipants = ({admins, group_participants, setGroupParticipants, setOpenParticipantsMenu}) => {
+    const {id} = useParams()
+    const authContext = useContext(AuthContext);
+    const [userID, setUserID] = useState('')
+    const [error, setError] = useState('')
+    useEffect(() => {
+        setUserID(authContext.UserID());
+    }, [authContext])
+    const handleRemoveUser = (userID) => {
+        console.log("DELETED USER ", userID);
+        Axios.delete(`http://localhost:8000/api/conversation/${id}/remove-user-from-group/${userID}`)
+        .then((res) => {
+            setGroupParticipants((prevParticipants) => prevParticipants.filter((participant) => participant._id !== userID))
+            Axios.post(`http://localhost:8000/api/conversation/${id}/send-message`, {
+                content: res.data.message,
+                isFromSystem: true,
+            })
+            .then(() => {})
+            .catch(err => console.log(err))
+            
+        }).catch((err) => {
+            console.log(err);
+            setError(err.response.data);
+        })
+    }
+    return (
+        <div className='blur'>
+            <div className="GroupParticipants">
+            <div className='close_div'>
+                    <h2>Group participants</h2>
+                    <span onClick={() => setOpenParticipantsMenu(false)}><CloseIcon /></span>
+                </div>
+                <div className="participants">
+                    {group_participants?.map(participant => (
+                    <div className='user_result participant_result' key={participant._id}>
+                        <div>
+                            <div className='avatar'>{participant.avatar ? "still not" : <Avatar {...stringAvatar(`${participant.first_name} ${participant.last_name}`)} />}</div>
+                            <div className='user_info'>
+                                <p className='fullName'>{participant.first_name} {participant.last_name} 
+                                {userID === participant._id && <span className='sign'>me</span>}
+                                {admins.includes(participant._id) && <span className='sign'>group admin</span>}
+
+                                </p>
+                                <p className='user_email'>{participant.email}</p>
+                            </div>
+                        </div>
+                        {admins.includes(userID) && userID !== participant._id && (
+                        <button onClick={() => handleRemoveUser(participant._id)} className='kick'>kick</button>
+                        )}
+                  </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default GroupParticipants;
