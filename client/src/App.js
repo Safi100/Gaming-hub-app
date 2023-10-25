29df5ca98c9502bdb521cdd4c60ea0e08539e889
Safi Nafi <safinafi12@gmail.com>
2023-10-25 04:35:15 +0300

    Create lazy load pages with Suspense

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, {lazy, Suspense, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// pages
-import Register from './pages/Auth/Register';
-import Login from './pages/Auth/Login';
-import ForgotPassword from './pages/Auth/ForgotPassword';
-import ResetPassword from './pages/Auth/ResetPassword';
-import EmailVerify from './pages/Auth/EmailVerify';
-import Chat from './pages/Chat/Chat';
-
-
 import axios from 'axios';
+
+import Navbar from './components/navbar/Navbar';
+
+// pages
+const  Home = lazy(() => import('./pages/Home/Home'));
+const  Register = lazy(() => import('./pages/Auth/Register'));
+const  Login = lazy(() => import('./pages/Auth/Login'));
+const  ForgotPassword = lazy(() => import('./pages/Auth/ForgotPassword'));
+const  ResetPassword = lazy(() => import('./pages/Auth/ResetPassword'));
+const  EmailVerify = lazy(() => import('./pages/Auth/EmailVerify'));
+const  Chat = lazy(() => import('./pages/Chat/Chat'));
+const  NewGame = lazy(() => import('./pages/Admin/NewGame/NewGame'));
+const  EditGame = lazy(() => import('./pages/Admin/EditGame/EditGame'));
 
 axios.defaults.withCredentials = true;
 
-
 function App() {
   return (
+  <>
+    <Navbar />
+    <Suspense>
         <Routes>
-          {/* <Route path="/" exact element={} /> */} {/* Index page... later */}
+          <Route path="/" exact element={<Home />} />
           <Route path="/register" element={<Register /> } />
           <Route path="/login" element={<Login /> } />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/chat/:id" element={<Chat />} />
-          <Route path="/reset-password/:id/:token" element={<ResetPassword />} />
+          <Route path="/chat" element={<Chat />} /> {/* protected route... later */}
+          <Route path="/admin/add-new-game" element={<NewGame />} />
+          <Route path="/admin/edit-game/:id" element={<EditGame />} />
+          <Route path="/chat/:id" element={<Chat />} /> {/* protected route... later */}
+          <Route path="/reset-password/:id/:token" element={<ResetPassword />} /> 
           <Route path="/verify-email/:id/:token" element={<EmailVerify />} />
-          {/* <Route path='*' element={} />  */}  {/* Not found page... later */}
+          <Route path='*' element={<p>There's nothing here: 404!</p>} />  {/* Not found page... later */}
         </Routes>
+    </Suspense>
+  </>
   );
 }
 
