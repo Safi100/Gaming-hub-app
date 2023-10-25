a2c5dbc67243349682008823cf7ec4fc7223793f
Safi Nafi <safinafi12@gmail.com>
2023-10-25 03:20:12 +0300

    update authMiddleware and create isAdmin middleware

--- api/middleware.js
+++ api/middleware.js
@@ -1,15 +1,29 @@
 const jwt = require('jsonwebtoken');
+const User = require('./models/user.model');
+const HandleError = require('./utils/HandleError');
 
 module.exports.authMiddleware = (req, res, next) => {
+  try {
     const token = req.headers.authorization || req.cookies.access_token;
-    if (!token) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    }
+    if (!token) throw new HandleError('You must log in to access this', 401);
+    
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
+    if (err) throw new HandleError('Invalid token', 401);
     req.user = decoded;
     next();
   });
+  }catch(e) {
+    next(e);
+  }
+
 };
+module.exports.isAdmin = async (req, res, next) => {
+  try{
+    const user = await User.findById(req.user.id);
+    if(!user.isAdmin) throw new HandleError('You are not allowed to access this', 403)
+    next();
+  }catch(e){
+    next(e);
+  }
+
+}
