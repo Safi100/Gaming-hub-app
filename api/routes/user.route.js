4afd89024f1c3436aeaccab11bdc0bc637a5ad32
Safi Nafi <safinafi12@gmail.com>
2023-10-25 04:04:45 +0300

    add more routes to user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { searchForUsers } = require('../controllers/user.controller')
 const router = express.Router({mergeParams: true})
+const { searchForUsers, fetchUserDataProfile, fetchCurrentUser, checkAuth } = require('../controllers/user.controller')
+const { authMiddleware, isAdmin } = require('../middleware')
 
+router.get('/currentUser', authMiddleware, fetchCurrentUser)
 router.post('/search', searchForUsers)
-
+router.get('/:id', fetchUserDataProfile)
 
 module.exports = router
