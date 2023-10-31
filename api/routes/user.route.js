0f96ef538452f7091b7361f0e3f09e650c5f4aa6
Safi Nafi <safinafi12@gmail.com>
2023-10-31 18:39:08 +0200

    create follow user route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router({mergeParams: true})
-const { searchForUsers, fetchUserDataProfile, fetchCurrentUser, checkAuth } = require('../controllers/user.controller')
+const { searchForUsers, fetchUserDataProfile, fetchCurrentUser, followUser } = require('../controllers/user.controller')
 const { authMiddleware, isAdmin } = require('../middleware')
 
 router.get('/currentUser', authMiddleware, fetchCurrentUser)
 router.post('/search', searchForUsers)
-router.get('/:id', fetchUserDataProfile)
+router.get('/profile/:id', fetchUserDataProfile)
+router.post('/follow-user/:id', authMiddleware, followUser)
 
 module.exports = router
