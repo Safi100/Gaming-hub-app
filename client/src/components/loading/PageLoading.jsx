7e4d88392049fec8fa21b8dda94a870e5fa33416
Safi Nafi <safinafi12@gmail.com>
2023-11-02 21:24:17 +0200

    modify page loading

--- client/src/components/loading/PageLoading.jsx
+++ client/src/components/loading/PageLoading.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import Spinner from 'react-bootstrap/Spinner';
 import './loading.css'
  
 const PageLoading = () => {
     return (
-        <div className='page_loading'>
-            <Spinner animation="grow" variant="primary" />
-            <Spinner animation="grow" variant="secondary" />
-            <Spinner animation="grow" variant="success" />
-            <Spinner animation="grow" variant="danger" />
-            <Spinner animation="grow" variant="warning" />
-            <Spinner animation="grow" variant="info" />
-            <Spinner animation="grow" variant="light" />
-            <Spinner animation="grow" variant="dark" />
+        <div class="page_loading">
+            <div className="loading_container">
+                <div class="parallel"></div>
+                <div class="parallel"></div>
+                <div class="parallel"></div>
+            </div>
         </div>
     );
 }
