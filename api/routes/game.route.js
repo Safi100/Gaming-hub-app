e4c29a5a96ad0ecfe243573a51e67bd2c41c70ae
Safi Nafi <safinafi12@gmail.com>
2023-11-03 23:00:45 +0200

    add more game route

--- api/routes/game.route.js
+++ api/routes/game.route.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { createGame, fetchAllGames, fetchGameToEdit, updateGame} = require('../controllers/game.controller')
+const { createGame, fetchAllGames, fetchGameToEdit, updateGame, toggle_favorite_game} = require('../controllers/game.controller')
 const router = express.Router({mergeParams: true})
 const { authMiddleware, isAdmin } = require('../middleware')
 const multer = require('multer')
@@ -10,5 +10,6 @@ router.get('/', fetchAllGames)
 router.post('/', authMiddleware, isAdmin, upload.fields([{ name: 'mainPhoto', maxCount: 1 }, { name: 'coverPhoto', maxCount: 1 }]), createGame)
 router.put('/:id', authMiddleware, isAdmin, upload.fields([{ name: 'mainPhoto', maxCount: 1 }, { name: 'coverPhoto', maxCount: 1 }]), updateGame)
 router.get('/fetch-game-to-edit/:id', authMiddleware, isAdmin, fetchGameToEdit)
+router.post('/add-to-favorite/:id', authMiddleware, toggle_favorite_game)
 
 module.exports = router
