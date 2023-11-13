409a20d1e79dd31938c457e1719486e357011ea7
Safi Nafi <safinafi12@gmail.com>
2023-11-13 20:33:56 +0200

    Create edit profile and remove avatar

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const HandleError = require('../utils/HandleError');
 const mongoose = require('mongoose');
 const User = require('../models/user.model');
+const {cloudinary} = require('../utils/cloudinary');
 
 module.exports.fetchCurrentUser = async (req, res, next) => {
   try{
@@ -71,6 +72,54 @@ module.exports.fetchUserDataProfile = async (req, res, next) => {
       next(e);
   }
 }
-// todo
-module.exports.editUserDataProfile = async (req, res, next) => {}
-// module.exports. = async (req,
+module.exports.editUserDataProfile = async (req, res, next) => {
+  try{
+    // Helper function to capitalize a string
+    const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1)
+    let user = await User.findById(req.user.id).select(['-isVerified', '-password', '-updatedAt'])
+    .populate({path: 'followers', select: ['first_name', 'last_name', 'avatar', 'email', 'isAdmin']})
+    const {first_name, last_name, gender, bio} = req.body
+
+    // Update the user document
+    user.first_name = capitalize(first_name.trim());
+    user.last_name = capitalize(last_name.trim());
+    user.gender = gender === null ? "" : capitalize(gender.trim());
+    user.bio = bio;
+    if(req.file){
+      // delete prev avatar and set the new avatar
+      if (user.avatar && user.avatar.filename) await cloudinary.uploader.destroy(user.avatar.filename);
+      // Set the new avatar details
+      user.avatar = { url: req.file.path, filename: req.file.filename };
+    }
+    await user.save()
+    res.status(200).json(user);
+  }catch(e){
+    if (req.file) {
+      // Delete image from Cloudinary
+      await cloudinary.uploader.destroy(req.file.filename);
+    }
+    console.log(e);
+    next(e)
+  }
+}
+module.exports.removeProfilePicture = async (req, res, next) => {
+  try{
+    const user = await User.findById(req.user.id)
+    if (!user.avatar || !user.avatar.url || !user.avatar.filename) {
+      throw new HandleError(`You already do not have a profile picture`, 404);
+    }
+    await cloudinary.uploader.destroy(user.avatar.filename);
+    user.avatar = null
+    await user.save()
+    res.status(200).json(user.avatar);
+  }catch(e){
+    next(e)
+  }
+}
+module.exports.changePassword = async (req, res, next) => {
+  try{
+    
+  }catch(e){
+    next(e)
+  }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router({mergeParams: true})
-const { searchForUsers, fetchUserDataProfile, fetchCurrentUser, follow_unfollow_user } = require('../controllers/user.controller')
+const { searchForUsers, fetchUserDataProfile, fetchCurrentUser, follow_unfollow_user, editUserDataProfile, removeProfilePicture } = require('../controllers/user.controller')
 const { authMiddleware, isAdmin } = require('../middleware')
+const multer = require('multer')
+const { storage } = require('../utils/cloudinary')
+const upload = multer({storage})
 
 router.get('/currentUser', authMiddleware, fetchCurrentUser)
 router.post('/search', searchForUsers)
+router.delete('/remove-profile-picture', authMiddleware, removeProfilePicture)
+router.put('/edit-profile', authMiddleware, upload.single('avatar'), editUserDataProfile)
 router.get('/profile/:id', fetchUserDataProfile)
 router.post('/toggle-following-user/:id', authMiddleware, follow_unfollow_user)
 
