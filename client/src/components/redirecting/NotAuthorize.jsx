911eead5dabf6a349db3916dd803602619db0558
Safi Nafi <safinafi12@gmail.com>
2023-12-04 20:27:44 +0200

    ...

--- client/src/components/redirecting/NotAuthorize.jsx
+++ client/src/components/redirecting/NotAuthorize.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './redirecting.css'
+
 const NotAuthorize = () => {
     return (
         <div className='text-white text-center NotAuthorize'>
             <div>
                 <h2>This page is only for admins</h2>
                 <p>Go to <a href="/login">login </a> page and login as an admin, or return to <a href="/">home </a>page</p>
-                <p></p>
             </div>
         </div>
     );
