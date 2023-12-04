05fde746a4b03040a962cc25dfb994e51e4cbb96
Safi Nafi <safinafi12@gmail.com>
2023-12-04 20:37:19 +0200

    add new game route

--- api/routes/game.route.js
+++ api/routes/game.route.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const { createGame, fetchGamesToControl, fetchGameToEdit, updateGame, toggle_favorite_game, fetchGameProfile} = require('../controllers/game.controller')
+const { createGame, fetchGamesToControl, fetchGameToEdit, updateGame, toggle_favorite_game, fetchGameProfile, fetchGamesID} = require('../controllers/game.controller')
 const router = express.Router({mergeParams: true})
 const { authMiddleware, isAdmin } = require('../middleware')
 const multer = require('multer')
 const { storage } = require('../utils/cloudinary')
 const upload = multer({storage})
 
+router.get('/fetch-games-category', authMiddleware, isAdmin, fetchGamesID) // admin routes
 router.get('/fetch-games-to-control', authMiddleware, isAdmin, fetchGamesToControl) // admin routes
 router.get('/:id', fetchGameProfile)
 router.post('/', authMiddleware, isAdmin, upload.fields([{ name: 'mainPhoto', maxCount: 1 }, { name: 'coverPhoto', maxCount: 1 }]), createGame) // admin routes
