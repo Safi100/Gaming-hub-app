013377b684902d471bf24f1d1327889ca675b335
Safi Nafi <safinafi12@gmail.com>
2023-12-04 20:27:55 +0200

    add more routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, {lazy, Suspense, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
-
 import Navbar from './components/navbar/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // pages
 const Home = lazy(() => import('./pages/Home/Home'));
@@ -13,6 +13,7 @@ const ResetPassword = lazy(() => import('./pages/Auth/ResetPassword'));
 const EmailVerify = lazy(() => import('./pages/Auth/EmailVerify'));
 const Chat = lazy(() => import('./pages/Chat/Chat'));
 const NewGame = lazy(() => import('./pages/Admin/NewGame/NewGame'));
+const NewGiveAway = lazy(() => import('./pages/Admin/NewGiveAway/NewGiveAway'));
 const EditGame = lazy(() => import('./pages/Admin/EditGame/EditGame'));
 const UserProfile = lazy(() => import('./pages/userProfile/UserProfile'));
 const GamesControl = lazy(() => import('./pages/Admin/Games/Games'));
@@ -31,14 +32,13 @@ function App() {
           <Route path="/login" element={<Login /> } />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/chat" element={<Chat />} /> {/* protected route... later */}
+          <Route path="/admin/add-new-giveaway" element={<NewGiveAway />} />
           <Route path="/admin/add-new-game" element={<NewGame />} />
           <Route path="/admin/games-control" element={<GamesControl />} />
           <Route path="/admin/edit-game/:id" element={<EditGame />} />
           <Route path="/chat/:id" element={<Chat />} /> {/* protected route... later */}
           <Route path="/profile/:id" element={<UserProfile />} />
           <Route path="/game/:id" element={<GameProfile />} />
-          {/* <Route path="/game/:id" element={<UserProfile />} /> */} // todo: game topics
-          {/* <Route path="/game/:id" element={<UserProfile />} /> */} // todo: add new topic
           <Route path="/reset-password/:id/:token" element={<ResetPassword />} /> 
           <Route path="/verify-email/:id/:token" element={<EmailVerify />} />
           <Route path='*' element={<p>There's nothing here: 404!</p>} />  {/* Not found page... later */}
