9bed7c8bcc2f9f165d9688b5782045d46e2e6f87
Safi Nafi <safinafi12@gmail.com>
2023-12-14 06:45:13 +0200

    Create topic model

---
+++ api/models/topic.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const topicSchema = new Schema({
+    subject: {
+        type: String,
+        required: [true, 'heading is required']
+    },
+    topic_body: {
+        type: String,
+        required: [true, 'body is required']
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'author is required']
+    },
+    comments: [
+        {
+            author: {
+                type: Schema.Types.ObjectId,
+                ref: 'User'  
+            },
+            body: {
+                type: String,
+                required: [true, 'Body is required']
+            }
+        }
+    ]
+}, {timestamps: true})
+
+module.exports = mongoose.model('Topic', topicSchema);
