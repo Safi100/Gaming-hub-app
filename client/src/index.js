fd79595eb1fd55001fbd3a105a2646d0037c06c6
Safi Nafi <safinafi12@gmail.com>
2023-12-19 14:31:20 +0200

    add NotificationContextProvider

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
+import { NotificationContextProvider } from './context/NotificationContext';
 import { ChatContextProvider } from './context/ChatContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <AuthContextProvider>
-    <ChatContextProvider>
-      <App />
-    </ChatContextProvider>
+    <NotificationContextProvider>
+      <ChatContextProvider>
+        <App />
+      </ChatContextProvider>
+    </NotificationContextProvider>
   </AuthContextProvider>
   </BrowserRouter>
 );
