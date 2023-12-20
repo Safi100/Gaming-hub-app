7be5a50179a43ee1c6f20822228690030cef5197
Safi Nafi <safinafi12@gmail.com>
2023-12-20 18:47:19 +0200

    add index route

--- api/server.js
+++ api/server.js
@@ -57,83 +57,17 @@ const UserRoute = require('./routes/user.route');
 const GameRoute = require('./routes/game.route');
 const GiveAwayRoute = require('./routes/giveaway.route');
 const TopicRoute = require('./routes/topic.route');
+const IndexRoute = require('./routes/index.route');
 
 const { authMiddleware } = require('./middleware');
 
+app.use('/api/', IndexRoute)
 app.use('/api/auth/', AuthRoute)
 app.use('/api/conversation/', authMiddleware, ConversationRoute) // protected route
 app.use('/api/user/', UserRoute)
 app.use('/api/game/', GameRoute)
 app.use('/api/giveaway/', GiveAwayRoute)
 app.use('/api/topic/', TopicRoute)
-
-// General search
-const User = require('./models/user.model');
-const Game = require('./models/game.model');
-const giveAwayModel = require('./models/giveaway.model');
-
-app.get('/test', async (req, res) => {
-  await giveAwayModel.deleteMany();
-})
-
-app.post('/api/search', async (req, res, next) => {
-  try{
-    let data = []
-    // users
-    const {q} = req.query
-    if(q.length < 1) return 0;
-    const users = await User.find({
-      $or: [{
-          $expr: {
-            $regexMatch: {
-              input: {
-                $concat: ["$first_name", " ", "$last_name"] // Concatenate first_name and last_name
-              },
-              regex: new RegExp(q.trim(), "i") // Search for the full name
-            }
-          }
-      }]
-    }).select(['_id', 'avatar', 'first_name', 'last_name', 'isAdmin', 'email']);
-    // games
-    const games = await Game.find({title: { $regex: new RegExp(q.trim(), "i") }}).select(['title', 'main_photo', 'genres'])
-    // push all users and games to array and sort
-    for(let i = 0; i < users.length; i++){
-      data.push({
-        title: users[i].first_name + " " + users[i].last_name,
-        _id: users[i]._id,
-        avatar: users[i].avatar || null,
-        isAdmin: users[i].isAdmin,
-        email: users[i].email,
-        isUser: true,
-      })
-    }
-    for(let i = 0; i < games.length; i++){
-      data.push({
-        title: games[i].title,
-        _id: games[i]._id,
-        main_photo: games[i].main_photo,
-        genres: games[i].genres
-      })
-    }
-    // sort data (games and users) by title
-    data.sort((a, b) => {
-      const titleA = a.title.toUpperCase();
-      const titleB = b.title.toUpperCase();
-    
-      if (titleA > titleB) {
-        return 1; // Change the order here
-      }
-      if (titleA < titleB) {
-        return -1; // Change the order here
-      }
-      return 0;
-    });
-    // send data to client
-    res.status(200).json(data)
-  }catch(e){
-    next(e);
-  }
-})
 
 app.use((err, req, res, next) => {
     if(!err.message) err.message = 'Internal Server Error'
