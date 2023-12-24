5f4eed527e5ab6ee2d042b6e4dab9eef89c15697
Safi Nafi <safinafi12@gmail.com>
2023-12-24 20:47:33 +0200

    Check if user banned in app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React, { lazy, Suspense, useEffect, useState } from 'react';
+import React, { lazy, Suspense, useEffect, useState, useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
 import axios from 'axios';
 import Navbar from './components/navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -27,6 +28,8 @@ const TopicPage = lazy(() => import('./pages/topic/Topic'));
 const EditTopic = lazy(() => import('./pages/editTopic/EditTopic'));
 const NoitficationPage = lazy(() => import('./pages/notifications/Notifications'));
 const HelpPage = lazy(() => import('./pages/help/Help'));
+const BannedPage = lazy(() => import('./components/redirecting/bannedUsers'));
+const BannedUsersPage = lazy(() => import('./pages/Admin/BannedUsers/BannedUsers'));
 
 axios.defaults.withCredentials = true;
 
@@ -43,8 +46,25 @@ function App() {
     }
   }, [location]);
 
-  return (
-  <>
+  const authContext = useContext(AuthContext)
+  const [isBanned, setIsBanned] = useState(false); // New state to handle ban status
+  const [banDetails, setBanDetails] = useState('');
+  
+  useEffect(() => {
+    setIsBanned(authContext.isBanned); // Set the ban status based on the context
+    setBanDetails(authContext.banDetails); // Set the ban status
+  }, [authContext]);
+
+  // If the user is banned, render the banned message
+  if (isBanned && authContext.currentUser) {
+    return (
+      <>
+      {<BannedPage banDetails={banDetails} />}
+      </>
+    );
+  }
+    return (
+      <>
     {showNavbar && <Navbar />}
     <Suspense>
         <Routes>
@@ -56,14 +76,15 @@ function App() {
           <Route path="/giveaway" element={<Giveaways />} />
           <Route path="/notifications" element={<NoitficationPage />} /> {/* protected route...*/}
           <Route path="/chat" element={<Chat />} /> {/* protected route...*/}
-          <Route path="/admin/add-new-giveaway" element={<NewGiveAway />} />
-          <Route path="/admin/add-new-game" element={<NewGame />} />
-          <Route path="/admin/games-control" element={<GamesControl />} />
-          <Route path="/giveaway/my-giveaway" element={<MyGiveawayPage />} />
+          <Route path="/admin/banned-users" element={<BannedUsersPage />} /> {/* protected route...*/}
+          <Route path="/admin/add-new-giveaway" element={<NewGiveAway />} /> {/* protected route...*/}
+          <Route path="/admin/add-new-game" element={<NewGame />} /> {/* protected route...*/}
+          <Route path="/admin/games-control" element={<GamesControl />} /> {/* protected route...*/}
+          <Route path="/giveaway/my-giveaway" element={<MyGiveawayPage />} /> {/* protected route...*/}
           <Route path="/topic/:id" element={<TopicPage />} />
           <Route path="/giveaway/:id" element={<GiveawayProfile />} />
-          <Route path="/admin/edit-giveaway/:id" element={<EditGiveAway />} />
-          <Route path="/admin/edit-game/:id" element={<EditGame />} />
+          <Route path="/admin/edit-giveaway/:id" element={<EditGiveAway />} /> {/* protected route...*/}
+          <Route path="/admin/edit-game/:id" element={<EditGame />} /> {/* protected route...*/}
           <Route path="/chat/:id" element={<Chat />} /> {/* protected route... */}
           <Route path="/profile/:id" element={<UserProfile />} />
           <Route path="/game/:id" element={<GameProfile />} />
@@ -76,5 +97,4 @@ function App() {
   </>
   );
 }
-
 export default App;
